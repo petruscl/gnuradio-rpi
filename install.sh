5ba1f84807b118b22a0efc36a6679844218eaab9
Petrus C. Lorena <petrus@techfab.com.br>
2023-03-30 10:52:17 -0300

    adicionadas cores para facilitar ver os comandos no flood do terminal

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-echo "fazendo update..."
+
+# Color variables
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+# Clear the color after that
+clear='\033[0m'
+
+
+
+echo -e "${blue} ---------fazendo update... --------- ${clear}"
 sudo apt update
-echo "fazendo upgrade..."
+echo -e "${blue} ---------fazendo upgrade... --------- ${clear}"
 sudo apt upgrade -y
-echo "instalando cmake..."
+echo -e "${blue} --------- instalando cmake... --------- ${clear}"
 sudo apt install cmake
-echo "instalando libusb..."
+echo -e "${blue} --------- instalando libusb... --------- ${clear}"
 sudo apt install libusb-1.0-0-dev
-echo "instalando osmocom..."
+echo -e "${blue} --------- instalando osmocom... --------- ${clear}"
 git clone git://git.osmocom.org/rtl-sdr.git
 cd rtl-sdr/
 mkdir build
@@ -19,9 +32,9 @@ sudo ldconfig
 sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
 cd /etc
 ls
-echo "fazendo o blacklist"
+echo -e "${blue} --------- fazendo o blacklist --------- ${clear}"
 echo 'blacklist blacklist dvb_usb_rtl28xxu' | sudo tee - append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
-echo "instalando gnuradio"
+echo -e "${blue} --------- instalando gnuradio... --------- ${clear}"
 sudo apt install gnuradio libusb-1.0-0 gr-iqbal
 sudo apt-get install gqrx-sdr
-echo "tudo pronto!"
+echo -e "${blue} --------- tudo pronto! --------- ${clear}"
