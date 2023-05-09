4465a0645e8399e6825b559bd81c18232cc7b650
Luís Eduardo Brito <68358157+luissiebra@users.noreply.github.com>
2023-05-09 10:03:01 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,13 +13,13 @@ clear='\033[0m'
 
 
 echo -e "${blue} ---------fazendo update... --------- ${clear}"
-sudo apt update
+sudo apt update -y
 echo -e "${blue} ---------fazendo upgrade... --------- ${clear}"
 sudo apt upgrade -y
 echo -e "${blue} --------- instalando cmake... --------- ${clear}"
-sudo apt install cmake
+sudo apt install cmake -y
 echo -e "${blue} --------- instalando libusb... --------- ${clear}"
-sudo apt install libusb-1.0-0-dev
+sudo apt install libusb-1.0-0-dev -y
 echo -e "${blue} --------- instalando osmocom... --------- ${clear}"
 git clone git://git.osmocom.org/rtl-sdr.git
 cd rtl-sdr/
@@ -35,6 +35,6 @@ ls
 echo -e "${blue} --------- fazendo o blacklist --------- ${clear}"
 echo -e'blacklist dvb_core\nblacklist dvb_usb_rtl2832u\nblacklist dvb_usb_rtl28xxu\nblacklist dvb_usb_v2\nblacklist r820t\nblacklist rtl2830\nblacklist rtl2832\nblacklist rtl2832_sdr\nblacklist rtl2838\nblacklist rtl8192cu\nblacklist rtl8xxxu\ninstall dvb_core /bin/false\ninstall dvb_usb_rtl2832u /bin/false\ninstall dvb_usb_rtl28xxu /bin/false\ninstall dvb_usb_v2 /bin/false\ninstall r820t /bin/false\ninstall rtl2830 /bin/false\ninstall rtl2832 /bin/false\ninstall rtl2832_sdr /bin/false\ninstall rtl2838 /bin/false\ninstall rtl8192cu /bin/false\ninstall rtl8xxxu /bin/false' | sudo tee - append /etc/modprobe.d/blacklist-rtlsdr.conf 
 echo -e "${blue} --------- instalando gnuradio... --------- ${clear}"
-sudo apt install gnuradio libusb-1.0-0 gr-iqbal
-sudo apt-get install gqrx-sdr
+sudo apt install gnuradio libusb-1.0-0 gr-iqbal -y
+sudo apt install gqrx-sdr -y
 echo -e "${blue} --------- tudo pronto! --------- ${clear}"
